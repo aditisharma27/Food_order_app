8a97a47a0ff230992ff98adee5d863cbf93022d6
Aditi Sharma <aditisharmaas2701@gmail.com>
2023-06-22 18:03:04 +0530

    Deployed changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,11 @@
 import { Fragment } from 'react';
 import HeaderCartButton from './HeaderCartButton';
-import soyaChaapImage from '../../assets/soyaChaap.webp';
-import TawapaneerImage from '../../assets/Tawa_paneer.webp';
+import soyaChaapImage from '../../assets/soyaChaap.jpg';
+import TawapaneerImage from '../../assets/Tawa_paneer.jpg';
 import TikkaImage from '../../assets/TikkaImage.jpg';
-import Tawa_chaap from '../../assets/Tawa_chaap.webp';
+import Tawa_chaap from '../../assets/TawachaapwithrumaliRoti.jpg';
 import classes from './Header.module.css';
+import MalaiSoya_chaap from '../../assets/Malai_Soya_Chaap_Tikka.webp';
 
 const Header = (props) => {
   return (
@@ -17,14 +18,20 @@ const Header = (props) => {
         <img src={TikkaImage} alt='A table full of delicious food!' />
       </div>
       <div className={classes['side-image']}>
-        <img src={soyaChaapImage} alt='A table full of  food!' /></div>
+        <img src={soyaChaapImage} alt='A table full of  food!' /> 
+        </div>
       <div className={classes['side-image']}>
         <img src={TawapaneerImage} alt='A table full of  food!' /></div>
-      <div className={classes['right-image']}>
-        <img src={Tawa_chaap} alt='A table full of  food!' /></div>
+        <div className={classes['side-image']}>
+        <img src={MalaiSoya_chaap} alt='A table full of  food!' /></div>
       
+      <div className={classes['side-image']}>
+        <img src={Tawa_chaap} alt='A table full of  food!' /> </div>
     </Fragment>
+
+    
   );
 };
+
 
 export default Header;
